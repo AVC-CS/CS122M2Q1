d9211c7526ad26058743b1b3df8d88127acf7cf9
Kyuwoong Lee <kwooler@gmail.com>
2023-10-15 21:04:46 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+def split(number):
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+    return number
+
+
+def main():
+    # number = list(map(int, input().split()))
+    number = [35, 5, 10, 20, 40, 15]
+    number = split(number)
+    print(number)
+
+
+if __name__ == '__main__':
+    main()
